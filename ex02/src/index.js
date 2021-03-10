504e920c37b99150ed0473567851f8f5491a7b08
OldXary <purisevicharis@gmail.com>
2021-03-10 14:42:02 +0100

    Add ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+
+    // First coment in js
+
+    /* First multi-line coment;
+         Second multi-line coment;
+         End coment;
+    */
+
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;

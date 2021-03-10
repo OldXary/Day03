5cbc2814c55137e95f5821a9d96d43c2a9c59535
OldXary <purisevicharis@gmail.com>
2021-03-10 15:32:34 +0100

    Add ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+    
+    x = 10;
+    y = x;
+
+    
+    // Only change code above this line
+    
+    }
+    
+    console.log(main());
+    
+    module.exports = main;
